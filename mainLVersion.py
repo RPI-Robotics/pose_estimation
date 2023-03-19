de631de893c00bcbce4b49a04d180d1ff6d7a675
BensonCheng8787 <bensoncheng888@gmail.com>
2023-03-19 15:09:58 -0400

    Scaled grid to 12X16, image is currently reversed.

--- mainLVersion.py
+++ mainLVersion.py
@@ -9,7 +9,7 @@ pose = mpPose.Pose()
 
 cap = cv2.VideoCapture(0)
 pTime = 0
-# 2d array to track the position of the landmarks 50x50
+# 2d array to track the position of the landmarks 12X16
 arr = [['.' for i in range(16)] for j in range(12)]
 k=0
 while k<50:
@@ -25,7 +25,8 @@ while k<50:
             cx, cy = int(lm.x*w), int(lm.y*h)
             cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
             ##print(lm.y*h,lm.x*w)
-            arr[int(lm.y*h/120)][int(lm.x*w/35)] = 0 ##scale the input to fit on the grid
+            ##arr[int(lm.y*h/80)][int(lm.x*w/35)] = 0
+            arr[min(int(lm.y*h/80),11)][min(int(lm.x*w/35), 15)] = 0 ##scale the input to fit on the grid
             ##print(h, w)
 
     cTime = time.time()
@@ -37,6 +38,7 @@ while k<50:
     cv2.imshow("Image", img)
     cv2.waitKey(1)
 
+## image IS REVERSED
 # print array in grid format, the grid
 for i in range(len(arr)):
     for j in range(len(arr[i])):
